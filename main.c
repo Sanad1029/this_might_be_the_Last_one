832be2c0a2c025ad7e179b842a93b96069e67982
ee25mt007-ui <ee25mt007@iitdh.ac.in>
2025-09-02 11:56:30 +0530

    Updating the code using own macro and masking

--- main.c
+++ main.c
@@ -1,52 +1,48 @@
 #include <stdint.h>
-#include "tm4c123gh6pm.h"
 
-void delay(int n){
-    int i,j;
-    for(i=1;i<n;i++){
-       for(j=1;j<3180;j++){
-       }
-    }
-}
+
+#define SYSCTL_RCGC2_R      (*((volatile uint32_t *)0x400FE108))
+#define GPIO_PORTF_DATA_R   (*((volatile uint32_t *)0x400253FC))
+#define GPIO_PORTF_DIR_R    (*((volatile uint32_t *)0x40025400))
+#define GPIO_PORTF_DEN_R    (*((volatile uint32_t *)0x4002551C))
+#define GPIO_PORTF_LOCK_R   (*((volatile uint32_t *)0x40025520))
+#define GPIO_PORTF_CR_R     (*((volatile uint32_t *)0x40025524))
+#define GPIO_PORTF_PUR_R    (*((volatile uint32_t *)0x40025510))
+
+#define GPIO_LOCK_KEY       0x4C4F434B
+#define RED_LED     (1 << 1)
+#define BLUE_LED    (1 << 2)
+#define GREEN_LED   (1 << 3)
+#define SW1         (1 << 4)
+#define SW2         (1 << 0)
 
 int main(void) {
-SYSCTL_RCGC2_R = 0x00000020;;
-GPIO_PORTF_LOCK_R = 0x4C4F434B;
-GPIO_PORTF_CR_R = 0x1F;
-GPIO_PORTF_DIR_R = 0x0E;
-GPIO_PORTF_DEN_R = 0x1F;
-GPIO_PORTF_PUR_R = 0x11;
+    SYSCTL_RCGC2_R |= 0x20;
 
-int count=0;
+    GPIO_PORTF_LOCK_R = GPIO_LOCK_KEY;
+    GPIO_PORTF_CR_R |= (RED_LED | BLUE_LED | GREEN_LED | SW1 | SW2);
 
-while(1){
-if((GPIO_PORTF_DATA_R & 0x10)==0){
-    if(count==0){
-        GPIO_PORTF_DATA_R =0x02;
-        delay(50);
-        GPIO_PORTF_DATA_R &= ~0x04;
-        GPIO_PORTF_DATA_R &= ~0x08;
-        count++;
+    GPIO_PORTF_DIR_R |= (RED_LED | BLUE_LED);
+    GPIO_PORTF_DIR_R &= ~(SW1 | SW2| GREEN_LED);
+
+    GPIO_PORTF_DEN_R |= (RED_LED | BLUE_LED | GREEN_LED | SW1 | SW2);
+
+    GPIO_PORTF_PUR_R |= (SW1 | SW2);
+
+    while(1){
+        if((GPIO_PORTF_DATA_R & SW2) == 0){
+            GPIO_PORTF_DATA_R |= RED_LED;
+        }
+        else{
+            GPIO_PORTF_DATA_R &= ~ RED_LED;
+        }
+
+        if((GPIO_PORTF_DATA_R & SW1) == 0){
+            GPIO_PORTF_DATA_R |= BLUE_LED;
+        }
+        else{
+            GPIO_PORTF_DATA_R &= ~ BLUE_LED;
+        }
+
     }
-    else if (count==1){
-        GPIO_PORTF_DATA_R =0x08;
-        delay(50);
-        GPIO_PORTF_DATA_R &= ~0x02;
-        GPIO_PORTF_DATA_R &= ~0x04;
-        count++;
-    }
-    else if(count==2){
-        GPIO_PORTF_DATA_R =0x04;
-        delay(50);
-        GPIO_PORTF_DATA_R &= ~0x02;
-        GPIO_PORTF_DATA_R &= ~0x08;
-        count++;
-    }
-    else {
-        delay(50);
-        GPIO_PORTF_DATA_R &= ~0x0E;
-        count=0;
-    }
-}
-}
 }
