c329a5503421a515d1d5985b82cbe1846f4c9880
ee25mt007-ui <ee25mt007@iitdh.ac.in>
2025-09-30 23:53:36 +0530

    Lab07

--- main.c
+++ main.c
@@ -1,74 +1,57 @@
 #include <stdint.h>
 #include "tm4c123gh6pm.h"
 
-#define SYSCTL_BASE       0x400FE000
-#define GPIO_PORTF_BASE   0x40025000
-#define NVIC_BASE         0xE000E100
-#define SYSTICK_BASE      0xE000E010
-#define SYSCTL_RCGCGPIO   (*((volatile uint32_t *)(SYSCTL_BASE + 0x608)))
-#define GPIO_PORTF_DATA   (*((volatile uint32_t *)(GPIO_PORTF_BASE + 0x3FC)))
-#define GPIO_PORTF_DIR    (*((volatile uint32_t *)(GPIO_PORTF_BASE + 0x400)))
-#define GPIO_PORTF_DEN    (*((volatile uint32_t *)(GPIO_PORTF_BASE + 0x51C)))
-#define GPIO_PORTF_PUR    (*((volatile uint32_t *)(GPIO_PORTF_BASE + 0x510)))
-#define GPIO_PORTF_LOCK   (*((volatile uint32_t *)(GPIO_PORTF_BASE + 0x520)))
-#define GPIO_PORTF_CR     (*((volatile uint32_t *)(GPIO_PORTF_BASE + 0x524)))
-#define GPIO_PORTF_IS     (*((volatile uint32_t *)(GPIO_PORTF_BASE + 0x404)))
-#define GPIO_PORTF_IBE    (*((volatile uint32_t *)(GPIO_PORTF_BASE + 0x408)))
-#define GPIO_PORTF_IEV    (*((volatile uint32_t *)(GPIO_PORTF_BASE + 0x40C)))
-#define GPIO_PORTF_ICR    (*((volatile uint32_t *)(GPIO_PORTF_BASE + 0x41C)))
-#define GPIO_PORTF_IM     (*((volatile uint32_t *)(GPIO_PORTF_BASE + 0x410)))
-#define GPIO_PORTF_RIS    (*((volatile uint32_t *)(GPIO_PORTF_BASE + 0x414)))
-#define NVIC_EN0          (*((volatile uint32_t *)(NVIC_BASE + 0x000)))
-#define NVIC_ST_CTRL      (*((volatile uint32_t *)(SYSTICK_BASE + 0x00)))
-#define NVIC_ST_RELOAD    (*((volatile uint32_t *)(SYSTICK_BASE + 0x04)))
-#define NVIC_ST_CURRENT   (*((volatile uint32_t *)(SYSTICK_BASE + 0x08)))
-#define RED_LED    (1U << 1)   
-#define BLUE_LED   (1U << 2)   
-#define GREEN_LED  (1U << 3)   
-#define SW2        (1U << 0)   
-#define SW1        (1U << 4)   
+void SysTick_Handler(void);
+void GPIOF_Handler(void);
+void SysTick_Init(void);
+void PortF_Init(void);
 
-void GPIO_Handler(void) {
-    SYSCTL_RCGCGPIO |= (1U << 5);
-    while((SYSCTL_RCGCGPIO & (1U << 5)) == 0);
-    GPIO_PORTF_LOCK = 0x4C4F434B;
-    GPIO_PORTF_CR   = (RED_LED | BLUE_LED | GREEN_LED | SW1 | SW2);
-    GPIO_PORTF_DIR  = (RED_LED | BLUE_LED | GREEN_LED);
-    GPIO_PORTF_PUR  = (SW1 | SW2);
-    GPIO_PORTF_DEN  = (RED_LED | BLUE_LED | GREEN_LED | SW1 | SW2);
-    GPIO_PORTF_IS  &= ~(SW1 | SW2);   
-    GPIO_PORTF_IBE &= ~(SW1 | SW2);   
-    GPIO_PORTF_IEV &= ~(SW1 | SW2);   
-    GPIO_PORTF_ICR  =  (SW1 | SW2);   
-    GPIO_PORTF_IM  |=  (SW1 | SW2);  
-    NVIC_EN0 |= (1U << 30);
+int main(void) {
+    PortF_Init();
+    SysTick_Init();
+    
+    __asm(" CPSIE I"); /
+    
+    while(1) {
+        // Infinite loop
+    }
 }
 
-void SysTick_Init(uint32_t ticks) {
-    NVIC_ST_CTRL = 0;                
-    NVIC_ST_RELOAD = ticks - 1;      
-    NVIC_ST_CURRENT = 0;            
-    NVIC_ST_CTRL = 0x07;             
+void SysTick_Init(void) {
+    NVIC_ST_CTRL_R = 0;
+    NVIC_ST_RELOAD_R = 16000000 - 1; 
+    NVIC_ST_CURRENT_R = 0;
+    NVIC_ST_CTRL_R = 0x07; 
+}
+
+void PortF_Init(void) {
+    SYSCTL_RCGCGPIO_R |= 0x20;
+    while((SYSCTL_PRGPIO_R & 0x20)==0){};
+    
+    GPIO_PORTF_LOCK_R = 0x4C4F434B;
+    GPIO_PORTF_CR_R = 0x1F;
+    
+    GPIO_PORTF_DIR_R = 0x0E; 
+    GPIO_PORTF_DEN_R = 0x1F;
+    GPIO_PORTF_PUR_R = 0x11;
+    
+    GPIO_PORTF_IS_R  &= ~0x11;
+    GPIO_PORTF_IBE_R &= ~0x11;
+    GPIO_PORTF_IEV_R &= ~0x11;
+    GPIO_PORTF_ICR_R = 0x11;
+    GPIO_PORTF_IM_R  |= 0x11;
+    
+    NVIC_PRI7_R = (NVIC_PRI7_R & 0xFF3FFFFF) | 0x00A00000;
+    NVIC_EN0_R  |= 0x40000000;
 }
 
 void SysTick_Handler(void) {
-    GPIO_PORTF_DATA ^= GREEN_LED;   D
+    GPIO_PORTF_DATA_R ^= 0x02; 
 }
 
 void GPIOF_Handler(void) {
-    if (GPIO_PORTF_RIS & SW1) {
-        GPIO_PORTF_ICR = SW1;       
-        GPIO_PORTF_DATA ^= BLUE_LED;  
-    }
-    if (GPIO_PORTF_RIS & SW2) {
-        GPIO_PORTF_ICR = SW2;     
-        GPIO_PORTF_DATA ^= RED_LED;   
-    }
-}
-
-int main(void) {
-    GPIO_Handler();
-    SysTick_Init(8000000);
-    while(1) {
+    if(GPIO_PORTF_RIS_R & 0x01){
+        GPIO_PORTF_ICR_R = 0x01;
+        GPIO_PORTF_DATA_R ^= 0x04; 
     }
 }
